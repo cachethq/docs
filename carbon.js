95a12fe4c18a7288e9579772fa9fbe4bacbd496d
James Brooks <james@alt-three.com>
2024-12-03 13:35:11 +0000

    Implement Carbon

---
+++ carbon.js
@@ -0,0 +1,8 @@
+(function() {
+    var carbonScript = document.createElement('script');
+    carbonScript.async = true;
+    carbonScript.type = 'text/javascript';
+    carbonScript.src = '//cdn.carbonads.com/carbon.js?serve=CWYDE2QJ&placement=cachethqio';
+    carbonScript.id = '_carbonads_js';
+    document.body.appendChild(carbonScript);
+})();
